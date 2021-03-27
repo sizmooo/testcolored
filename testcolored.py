6a72f888e5c6df33bb04955cb95d17666aa08e69
sizmooo <39835424+sizmooo@users.noreply.github.com>
2021-03-28 01:48:34 +0300

    Add files via upload

---
+++ testcolored.py
@@ -0,0 +1,9 @@
+from colored import fg, bg, attr
+color1 = fg(2)
+color2 = fg(1)
+color3 = fg(3)
+
+usr = input(color3+' nav: ')
+pas = input(color3+' pas: ')
+print(color1+' '+usr)
+print(color2+' '+pas)
